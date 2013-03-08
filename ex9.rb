be6ad171eebaf0210409d9c205e992b86559370b
Anthony@BlackHole <zhouyaudee@163.com>
2013-03-08 23:35:07 +0800

    Exercise 9

---
+++ ex9.rb
@@ -0,0 +1,14 @@
+# Here's some new strange staff, remember type it exactly.
+
+days = "Mon Tue Wen Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\mAug\nSep\nOct\nNov\nDec"
+
+puts "Here are the days:", days
+puts "Here are the months:", months
+
+puts <<PARAGRAPH
+There's soemthing going on here.
+With the PRAGRAPH thing
+We'll be able to type as much as we like.
+Even 4 lines, if you want, or 5, or 6
+PARAGRAPH
