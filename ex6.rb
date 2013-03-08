5c814a5bc9552a391d41534a8390a9a7b8d970e5
Anthony@Deck <yaodi.zhou@gmail.com>
2013-03-08 13:14:36 +0800

    Exercise 6

---
+++ ex6.rb
@@ -0,0 +1,21 @@
+name1 = "Mary"
+name2 = "Joe"
+puts "Hello %s, where's %s?" % [name1,name2]
+puts "Hello #{name1}, where's #{name2}?"
+
+x = "There are #{10} types of people."
+binary = 'binary'
+do_not = "don't"
+y = "Those who knows #{binary} and those who #{do_not}."
+
+puts x
+puts y
+
+puts "I said that: #{x}"
+puts "I also said: #{y}"
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+puts joke_evaluation
+
