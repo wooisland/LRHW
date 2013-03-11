b1f02388c2ad31e342a846ac7d5036bce9bb1d71
Anthony@BlackHole <zhouyaudee@163.com>
2013-03-11 23:18:50 +0800

    Exercise 13

---
+++ ex13.rb
@@ -0,0 +1,8 @@
+#ruby ex12.rb
+
+first,second,third = ARGV
+
+puts "The script is called: #{$0}"
+puts "The first variable is: #{first}"
+puts "The second variable is: #{second}"
+puts "The third variable is: #{third}"
